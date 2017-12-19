c68f73cd18f848cbff39713b0f0c0cce88f6e07c
VolandLarsen <nitro1493@gmail.com>
2017-12-19 12:34:11 +0200

    Final

---
+++ source-js/main.js
@@ -0,0 +1,54 @@
+function addition(){
+  var num1 = 'x1'
+  var num2 = 'x2'
+  var result
+  result = num1 + num2
+  return result
+}
+
+function subtraction(){
+  var num1 = 'x1'
+  var num2 = 'x2'
+  var result
+  result = num1 - num2
+  return result
+}
+
+function multiplication(){
+  var num1 = 'x1'
+  var num2 = 'x2'
+  var result
+  result = num1 * num2
+  return result
+}
+
+function division(){
+  var num1 = 'x1'
+  var num2 = 'x2'
+  var result
+  if (0 < num2 < 0){
+    result = num1 / num2
+    return result
+  }
+}
+
+function power(){
+  var num1 = 'x1'
+  var num2 = 'x2'
+  var result
+  result = num1 ** num2
+  return result
+}
+
+function sqrt(){
+  var num1 = 'x1'
+  var num2 = 'x2'
+  var result
+  if (x1 <= 0){
+    result = num1 ** 1/num2
+    return result
+  }
+}
+
+
+
